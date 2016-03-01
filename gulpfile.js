57d7d94bbe1c7d7e1f7ce0a77bde8f02cca647b7
Marcel Dancak <dancakm@gmail.com>
2016-03-02 00:43:26 +0100

    Builded new demo

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ var connect = require('gulp-connect');
 var path = require('path');
 
 
-var TARGET = 'dist/v2/';
+var TARGET = 'dist/v3/';
 
 var DEV_JS = 'src/js/**/*.js';
 var DEV_HTML = 'src/views/**/*.html';
@@ -118,7 +118,7 @@ gulp.task('build', ['index-page', 'uglify', 'csss']);
 
 gulp.task('serve-deploy', function() {
   connect.server({
-    root: ['dist/v2'],
+    root: ['dist/v3'],
     port: 3300,
     livereload: true
   });
