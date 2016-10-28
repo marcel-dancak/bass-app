81c9ff4f2c4a1f30072454f649dd757cf2749cff
Marcel Dancak <dancakm@gmail.com>
2016-10-29 00:14:53 +0200

    Updated help manual - added drums

---
+++ src/js/help/drum-grid.js
@@ -0,0 +1,55 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('bd.help')
+    .controller('DrumSounds', DrumSounds);
+
+
+  function DrumSounds($scope, $element, $timeout) {
+
+    $scope.instructions = [
+      /* Create a first sound */
+      function() {
+        $scope.workspace.addSound(1, 1, 2, '2', 0.85);
+      },
+
+      function() {
+        $scope.workspace.addSound(1, 1, 2, '2', 0.4);
+      },
+      function() {
+        $scope.workspace.addSound(1, 1, 2, '2', 0);
+      },
+      // function() {
+      //   var volumes = [0.1, 0.2, 0.3, 0.4, 0.3, 0.2];
+      //   volumes.forEach(function(volume, index) {
+      //     $timeout(function() {
+      //       $scope.workspace.addSound(1, 1, 2, '2', volume);
+      //     }, (index+1)*150);
+      //   });
+      // },
+
+      function() {},
+      function() {
+        var volumes = [0.1, 0.2, 0.3, 0.4];
+        volumes.forEach(function(volume, index) {
+          $timeout(function() {
+            $scope.workspace.addSound(1, 1, 2, '2', volume);
+          }, (index+1)*200);
+        });
+      },
+      function() {
+        var volumes = [0.3, 0.2];
+        volumes.forEach(function(volume, index) {
+          $timeout(function() {
+            $scope.workspace.addSound(1, 1, 2, '2', volume);
+          }, (index+1)*250);
+        });
+      },
+      function() {
+        $scope.workspace.addSound(1, 1, 2, '2', 0);
+      }
+    ];
+  }
+
+})();
---
+++ src/js/help/drum-sheet.js
@@ -0,0 +1,78 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('bd.help')
+    .directive('bdHelpDrumSheet', drumSheet);
+
+  function drumSheet() {
+
+    var sheetTemplate = 
+      '<div class="help-drum-sheet">'+
+        '<div layout="column" class="drums-labels-container">'+
+          '<img ng-repeat="drum in workspace.track.instrument track by drum.name" ng-src="{{ ::drum.image }}">'+
+        '</div>'+
+        '<div class="beats-container" layout="row">'+
+          '<div flex '+
+            'ng-repeat="slide in workspace.beatSlides" '+
+            'ng-include="\'views/drums_board_slide.html\'" '+
+            'class="beat instrument-slide">'+
+          '</div>'+
+        '</div>'+
+      '</div>';
+
+    return {
+      restrict: 'A',
+      scope: {
+        config: '=bdHelpDrumSheet'
+      },
+      template: '',
+      controller: function($scope, $element, $mdCompiler, Drums, DrumSection, basicHandler) {
+        function createSlide(beat) {
+          return {
+            id: beat,
+            initialized: true,
+            visible: true,
+            beat: $scope.workspace.trackSection.beat(1, beat),
+            type: 'drums'
+          }
+        }
+        function addSound(bar, beat, subbeat, drum, volume) {
+          $scope.workspace.trackSection.subbeat(bar, beat, subbeat)[drum].volume = volume;
+        }
+        $scope.initializeWorkspace = function(config) {
+          var numbers = (config.timeSignature || '4/4').split('/');
+          var timeSignature = {
+            top: numbers[0],
+            bottom: numbers[1]
+          };
+          $scope.workspace = {
+            selected: basicHandler.selected,
+            section: {
+              timeSignature: timeSignature,
+              length: config.length || 1
+            },
+            track: {
+              instrument: Drums.Standard.slice(4)
+            }
+          };
+          $scope.workspace.trackSection = new DrumSection($scope.workspace.section);
+          $scope.workspace.beatSlides = [createSlide(1), createSlide(2)];
+          $scope.workspace.addSound = addSound;
+
+          var template = '<div ng-controller="{0}">'.format($scope.config.controller)+sheetTemplate+'</div>';
+          $mdCompiler.compile({
+            template: template
+          }).then(function(compileData) {
+            //attach controller & scope to element
+            var sheetElem = compileData.link($scope);//.$new(false)
+            $element.append(sheetElem);
+          });
+        };
+        $scope.initializeWorkspace($scope.config || {});
+      }
+    };
+  }
+
+
+})();
