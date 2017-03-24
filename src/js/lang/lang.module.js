86773274006f1d01e90f6993503770a19415d775
Marcel Dancak <dancakm@gmail.com>
2017-03-24 14:03:53 +0100

    Moved web page from master into the new branch

--- src/js/lang/lang.module.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  'use strict';
-  angular
-    .module('bd.lang', ['pascalprecht.translate'])
-    .config(function ($translateProvider) {
-      $translateProvider.preferredLanguage('en');
-      $translateProvider.fallbackLanguage('en');
-    });
-})();
